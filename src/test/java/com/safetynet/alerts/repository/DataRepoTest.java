5e204a44b5ec2b24acfe9b2a32b11370b82c83d6
Sukanya <sukanyarits@gmail.com>
2023-06-11 22:40:34 -0500

    added imports

--- src/test/java/com/safetynet/alerts/repository/DataRepoTest.java
+++ src/test/java/com/safetynet/alerts/repository/DataRepoTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DataRepo.class})
 public class DataRepoTest {
     @Autowired
     private DataRepo sut; // system under test (we are testing this class)
@@ -14,7 +16,7 @@ public class DataRepoTest {
     // TODO add the junit test for DataRepo
     @Test
     public void testBean(){
-        // assertThat(sut).isNotNull();
+        assertThat(sut).isNotNull();
 
 
     }
